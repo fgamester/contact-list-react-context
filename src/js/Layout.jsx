8e4268b9f5ac33f31ff474455792575e0b7a2703
fgamester <falvarez.munoz@hotmail.es>
2024-09-08 20:11:01 -0300

    ContactList added

--- src/js/Layout.jsx
+++ src/js/Layout.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import NewContact from "./views/NewContact";
+import NewContact from "./views/NewContact.jsx";
+import ContactList from "./views/ContactList.jsx";
 
 const Layout = () => {
     return (
         <div className="m-2">
-            <NewContact />
+            <ContactList />
         </div>
     );
 }
